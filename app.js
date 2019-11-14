dd38b3afaed12346c2938416bd9ae5d5e8de9224
raedeid <raededialrfou12345@gmail.com>
2019-11-14 14:36:39 +0200

    function4

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ function multiply(a, b) { //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -80,12 +80,14 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+   var sumation_of_array_1 = sum(sumArr[0],sumArr[1])[0]
+   var sumation_of_array_2 = sum(sumation_of_array_1,sumArr[2])[0]
+   return [sumation_of_array_2,`${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sumation_of_array_2} is their sum.`]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
